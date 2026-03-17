f8171b19682723139a3ebd9accd5f1ddd73ec167
ellen2007yuri <ellenyurisuzuki2010@gmail.com>
2026-03-17 18:19:30 -0300

    Add files via upload

---
+++ programacao full stack/aula4/aula5/arquivo.js
@@ -0,0 +1,20 @@
+window.alert("nclenfwb")
+console.log("dado escondido");
+
+
+
+let entrada = prompt("Entre com sua idade: ")
+console.log("Sua idade eh : ")
+console.log(entrada)
+
+
+entrada=parseInt(entrada);
+if (entrada===9){
+    console.log("igual a 9")
+}
+else if(entrada>9){
+     console.log("maior a 9")
+}
+else{
+     console.log("menor a 9")
+}
